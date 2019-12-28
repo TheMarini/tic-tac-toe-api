e8d9cd8c895500348571c32e8df28aed2769cbec
Bruno Marini <bruno.marini1@gmail.com>
2019-12-28 07:15:24 -0300

    Database methods [+][^] {read, write, overwrite}

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,31 @@ const jsonfile = require('jsonfile');
 
 const file = `${__dirname}/database.json`;
 
-exports.read = () => {
-	try {
-		return jsonfile.readFile(file);
-	} catch (e) {
-		console.error(e);
-	}
-};
+module.exports = {
+	read () {
+		try {
+			return jsonfile.readFile(file);
+		} catch (e) {
+			console.error(e);
+		}
+	},
+
+	async write (obj) {
+		try {
+			let database = await this.read();
+			database.push(obj);
+			this.overwrite(database);
+			return obj;
+		} catch (e) {
+			console.error(e);
+		}
+	},
+
+	overwrite (json) {
+		try {
+			return jsonfile.writeFile(file, json, { spaces: 2 });
+		} catch (e) {
+			console.error(e);
+		}
+	},
+}
