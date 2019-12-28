06f9661f7e6247f6a8f682d1c4a61679dd4a8803
Bruno Marini <bruno.marini1@gmail.com>
2019-12-28 03:08:11 -0300

    "Game" router [+]

---
+++ src/routes/v1/gameRouter.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const controller = require('./../../controllers/gameController.js');
+
+// Create
+router.post('/', async (req, res) => {
+
+});
+
+module.exports = router;
