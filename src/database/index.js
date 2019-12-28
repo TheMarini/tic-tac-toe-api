81531df27809b31f67de44d659b0f8edbcb7fa75
Bruno Marini <bruno.marini1@gmail.com>
2019-12-28 09:48:39 -0300

    Database [^] {methods names, var names}

--- src/database/index.js
+++ src/database/index.js
@@ -3,26 +3,40 @@ const jsonfile = require('jsonfile');
 const file = `${__dirname}/database.json`;
 
 module.exports = {
-	read () {
+	retrieve (id) {
 		try {
-			return jsonfile.readFile(file);
+			return jsonfile.readFile(file)
+				.then(rows => {
+					if (id) {
+						return rows.find( row => row.id === id );
+					} else {
+						return rows;
+					}
+				})
 		} catch (e) {
 			console.error(e);
 		}
 	},
 
-	async write (obj) {
+	async create (row) {
 		try {
-			let database = await this.read();
-			database.push(obj);
-			this.overwrite(database);
-			return obj;
+			let rows = await this.retrieve();
+			rows.push(row);
+			this.overwriteFile(rows);
+			return row;
 		} catch (e) {
 			console.error(e);
 		}
 	},
 
-	overwrite (json) {
+	async update (newRow) {
+		let rows = await this.retrieve();
+		rows[rows.findIndex( row => row.id === newRow.id )] = newRow;
+		this.overwriteFile(rows);
+		return newRow;
+	},
+
+	overwriteFile (json) {
 		try {
 			return jsonfile.writeFile(file, json, { spaces: 2 });
 		} catch (e) {
