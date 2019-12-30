e347e44ec2d589c57fd4e113139c037c7881a462
Bruno Marini <bruno.marini1@gmail.com>
2019-12-30 01:38:04 -0300

    Webpack config [*] {wrong "__dirname" and "__filename" scope}

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 module.exports = {
 	mode: "production",
 	target: 'node',
+	node: {
+	  __dirname: false,
+	  __filename: false
+	},
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
